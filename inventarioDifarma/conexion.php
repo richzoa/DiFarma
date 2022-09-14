7aca8be7eecb9f56f742c32dda90c2e926b0913a
DANIEL2610 <112772259+DANIEL2610@users.noreply.github.com>
2022-09-14 16:22:37 -0500

    Inventario falta el css
    Felipe corra eso y agreguele el css no boostrap o si pero que corra sin internet

---
+++ inventarioDifarma/conexion.php
@@ -0,0 +1,16 @@
+<?php 
+//Se crea una clase que almacene la conexion a la base de datos
+ class Db{
+//Si existe algun error la siguiente funcion manda una alerta
+ 		private static $conexion=NULL;
+		private function __construct(){} 
+
+		public static function conectar(){
+			$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
+			//Se establece la conexion a la base de datos dependiendo el puerto y la base de datos y el perfil que se maneje,ademas, una contraseña si se le establecio alguna
+			self::$conexion=new PDO('mysql:host=localhost;dbname=inventario','root','',$pdo_options);
+			//retorna la verificacion de conexion a la base de datos
+			return self::$conexion;
+		}
+		}
+ ?>
